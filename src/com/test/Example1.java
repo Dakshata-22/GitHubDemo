5eb8860030bcf1e31d8b1f26c238a7de32380f0f
Dakshata-22 <bhanjidakshata22@gmail.com>
2022-12-26 21:24:41 +0530

    First commit : ArrayList program

---
+++ src/com/test/Example1.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import java.util.ArrayList;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+		ArrayList list = new ArrayList();
+		list.add(10);
+		list.add("Dakshata");
+		list.add("bhanji");
+		System.out.println(list);
+		
+	}
+}
