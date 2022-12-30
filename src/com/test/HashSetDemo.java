78241db3b3fab0a21f99dde0483d859f669970f9
Dakshata-22 <bhanjidakshata22@gmail.com>
2022-12-30 23:33:41 +0530

    HashSet demo code added

---
+++ src/com/test/HashSetDemo.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import java.util.HashSet;
+
+public class HashSetDemo {
+
+	public static void main(String[] args) {
+		
+		HashSet<Integer> hs =  new HashSet<Integer>();
+		hs.add(10);
+		hs.add(20);
+		hs.add(30);
+		System.out.println(hs);
+	}
+}
